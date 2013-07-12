4248ecc303bbcac1aa896e3cb383feaa4c2b1d83
Sergio Garcia <s3rgio.gr@gmail.com>
2013-07-12 16:03:45 +0000

    Added index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  buf = new Buffer(fs.readFileSync('index.html');
+  buf = new Buffer(fs.readFileSync('index.html'));
   response.send(buf.toString());
 });
 
